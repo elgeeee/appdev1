59e48f4983ea0b4bd021122444807004cf450d64
elgeeee <ellowlayn@gmail.com>
2025-08-27 15:22:20 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>About Me</h1>
+
+        <p><strong>Name:</strong> Lorlyn Grace Boiser</p>
+        <p><strong>Course/Year:</strong> BS Information Systems 3rd Year</p>
+        <p><strong>Fun Fact:</strong> I can read a whole book in one day and sleep comfortably without moving.</p>
+
+        <hr />
+
+        <h2>Why I Want to Learn React</h2>
+        <p>
+          I want to learn React because it is a widely used technology in web
+          development. It makes creating user interfaces more efficient and easier to manage. 
+          By studying React, I can improve the way I design and gain practical skills that are valuable for both academic
+          projects and future career opportunities.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
