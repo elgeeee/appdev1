a1c6807d4d53855cb62e28308254693d092aaf73
elgeeee <ellowlayn@gmail.com>
2025-09-05 22:54:54 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,12 @@
+let myAge = 21
+const myName = "Lyncie"
+var course = "Gen Math"
+
+myAge = 22 
+
+console.log(myAge, myName, course)
+
+var name = "Lor" 
+var age = 20
+age = 20
+console.log(name, age)
