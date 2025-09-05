84eb962b65dce26670730c73739c9cdd80ac9985
elgeeee <ellowlayn@gmail.com>
2025-09-05 22:45:49 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,23 @@
+const name = "Lor"
+let age = 20
+var isStudent = true
+
+console.log("Name:", name)
+console.log("Age:", age)
+console.log("Is Student:", isStudent)
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+
+// Arithmetic
+let a = 15
+let b = 9
+console.log("Addition:", a + b)
+console.log("Subtract:", a - b)
+console.log("Multiplication:", a * b)
+console.log("Division:", a / b)
+
+// Comparison
+console.log("5" == 5)  
+console.log("5" === 5) 
