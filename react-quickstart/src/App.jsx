1e1743e10da7e4a71cdd6a4c169bf3a3c28645dc
elgeeee <ellowlayn@gmail.com>
2025-09-17 14:59:25 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,26 +1,24 @@
 import './App.css'
 import { useState } from 'react'
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Banana', isFruit: true, id: 2 },
-  { title: 'Mango', isFruit: true, id: 3 },
-];
-
-function App() {
-  const listItems = products.map(product =>
-  <li 
-    key={product.id}
-    style={{
-      color: product.isFruit ? 'magenta' : 'white'
-    }}
-  >
-    {product.title}
-  </li>
-);
+function MyButton() {
+  function handleClick() {
+    alert('Oops, You clicked me!');
+  }
 
   return (
-    <ul>{listItems}</ul>
+    <button onClick={handleClick}>
+      Click me now!
+    </button>
+  );
+}
+
+
+function App() {
+  return (
+  <>
+    <MyButton />
+  </>
   )
 }
 
