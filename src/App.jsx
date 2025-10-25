785964ae19825dc58a26df3381d7e2abd038f225
elgeeee <ellowlayn@gmail.com>
2025-10-25 17:58:56 +0800

    Done with when and how to create multi-component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Profile from './Profile.jsx'
 
 export default function Gallery() {
   return (
