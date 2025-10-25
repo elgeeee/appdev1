ac2c636372af533f3fe053b51bc0bccca3dcc63e
elgeeee <ellowlayn@gmail.com>
2025-10-25 18:19:28 +0800

    Done with how to add markup to JavaScript with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import './App.css'
 
+{/*HTML markup*/}
+// export default function TodoList() {
+//   return (
+//     // This doesn't quite work!
+//     <h1>Hedy Lamarr's Todos</h1>
+//     <img
+//       src="https://i.imgur.com/yXOvdOSs.jpg"
+//       alt="Hedy Lamarr"
+//       class="photo"
+//     >
+//     <ul>
+//       <li>Invent new traffic lights
+//       <li>Rehearse a movie scene
+//       <li>Improve spectrum technology
+//     </ul>
+//   );
+// }
+
+
 export default function TodoList() {
   return (
     <>
