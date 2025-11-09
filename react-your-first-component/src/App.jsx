ada9c977f11775f934d8bc4e017b2ec19cdcfc48
elgeeee <ellowlayn@gmail.com>
2025-11-09 20:01:37 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  )
+}
