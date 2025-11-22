c5c3af534f82a60bf86764d35186e929e8dce57c
elgeeee <ellowlayn@gmail.com>
2025-11-22 14:40:22 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
 
-export default function Avatar() {
-  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const description = 'Gregorio Y. Zara';
+//Ouput: Gregorio Y. Zara's To Do List
+// export default function TodoList() {
+//   const name = 'Gregorio Y. Zara';
+//   return (
+//     <h1>{name}'s To Do List</h1>
+//   );
+// }
+
+// Output: To Do List for Saturday
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
   return (
-    <img
-      className="avatar"
-      src={avatar}
-      alt={description}
-    />
+    <h1>To Do List for {formatDate(today)}</h1>
   );
 }
+
